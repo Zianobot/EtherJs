433fb693855a7a3aca41004672a6e248811b0355
Alfredo <graziano.alfredo34@yahoo.com>
2022-03-08 23:03:19 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { ContractService } from './contract.service';
-import { ethers } from 'ethers';
-import { parseUnits } from 'ethers/lib/utils';
 
 @Component({
   selector: 'app-root',
@@ -11,29 +9,22 @@ import { parseUnits } from 'ethers/lib/utils';
 })
 export class AppComponent {
 
-  title = 'EtherJs';
-  balance = 0;
-  formattedBalance = '';
-  userAddress: string = '';
-  show: boolean = false;
+  public title = 'EtherJs';
+  //public balance = 0;
+  //public formattedBalance = '';
+  public userAddress: string = '';
+  public show: boolean = false;
 
   //smartAuction
   public highestBid = 0;
-  
-  public amount: number = 0;
   public auctionAlreadyEnded: boolean = false;
-
-  private ehtPrecision = 10 ** 18;
-  private smartAuction: any;
-  private account: any;
-
-
+  public amount: number = 0;
+  
   constructor(private loginService: LoginService, private contractService: ContractService) {}
   
   async ngOnInit() {
-    
     //this.fetchCurrentValues();
-
+    //da fare solo dopo aver effettuato il login
   }
 
   async login() {
@@ -46,46 +37,25 @@ export class AppComponent {
     this.show = true;
     this.userAddress = this.loginService.userAddress;
   }
-/*
- async readContract() {
-   this.contractService.getContractInfo();
-  }
 
- async modifyContract() {
-   this.contractService.storeNumber();
- }
-
-*/
-
-  //contratto smartAuction
-
-  public async fetchCurrentValues() {
-      this.smartAuction = new ethers.Contract(this.contractService.contractAddress, this.contractService.contractAbi, this.loginService.provider);
-      this.highestBid =  await this.smartAuction.highestBid() / this.ehtPrecision;
-      this.auctionAlreadyEnded = await this.smartAuction.auctionAlreadyEnded();
+  //CONTRATTO SMARTAUCTION
+  
+  public async fetchContractValues() {
+    await this.contractService.fetchCurrentValues();
+    this.highestBid = this.contractService.highestBid;
+    this.auctionAlreadyEnded = this.contractService.auctionAlreadyEnded;
   }
 
   public async bid() {
-    this.smartAuction = new ethers.Contract(this.contractService.contractAddress, this.contractService.contractAbi, this.loginService.signer);
-    let amountConverted: string = (this.amount).toString();
-    await this.smartAuction.bid({
-      from: this.loginService.userAddress, 
-      value: parseUnits(amountConverted),
-      gasPrice: this.loginService.provider.getGasPrice(),
-      gasLimit: ethers.utils.hexlify(100000)
-    });
+    this.contractService.bid(this.amount);
   }
 
   public async withdraw() {
-    this.smartAuction = new ethers.Contract(this.contractService.contractAddress, this.contractService.contractAbi, this.loginService.signer);
-    let withdrawSuccess = await this.smartAuction.withdraw({
-      from: this.loginService.userAddress
-    });
-    console.log(withdrawSuccess);
+    this.contractService.withdraw();
   }
 
-  endAuction() {
-
+  public async endAuction() {
+    this.contractService.endAuction();
   }
 
 }
--- src/app/contract.service.ts
+++ src/app/contract.service.ts
@@ -1,35 +1,50 @@
-import { LoginService } from './login.service';
 import { Injectable } from '@angular/core';
+import { LoginService } from './login.service';
 import { ethers } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContractService {
 
-  contractIstance: any;
-/*
-  readonly contractAddress = '0x2388191BEd7f5fA4a89043BD33BD3e4A828e2b94';
-  readonly contractAbi = [{"inputs":[],"name":"retrieve","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"num","type":"uint256"}],"name":"store","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-*/
+  private smartAuction: any;
+  private ehtPrecision = 10 ** 18;
+  
+  public highestBid = 0;
+  public auctionAlreadyEnded: boolean = false;
 
   readonly contractAddress = '0x3601C5007Fcbaf7200ece3BfDc61c5578961ad80';
-  readonly contractAbi = [{"inputs":[{"internalType":"uint256","name":"_biddingTime","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"winner","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"AuctionEnded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"bidder","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"HighestBidIncreased","type":"event"},{"inputs":[],"name":"auctionAlreadyEnded","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"auctionEnd","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"beneficiary","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"bid","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"endAuction","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"highestBid","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"highestBidder","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"payable","type":"function"}]
-  num: any;
-  
-  constructor(private loginService: LoginService) { 
-    
-  }
-/*
-  async getContractInfo() {
-    this.contractIstance = new ethers.Contract(this.contractAddress, this.contractAbi, this.loginService.provider);
-    this.num = await this.contractIstance.retrieve();
-    console.log(this.num.toString());
+  readonly contractAbi = [{"inputs":[{"internalType":"uint256","name":"_biddingTime","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"winner","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"AuctionEnded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"bidder","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"HighestBidIncreased","type":"event"},{"inputs":[],"name":"auctionAlreadyEnded","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"auctionEnd","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"beneficiary","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"bid","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"endAuction","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"highestBid","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"highestBidder","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"payable","type":"function"}];
+
+  constructor(private loginService: LoginService) {}
+
+  public async fetchCurrentValues() {
+    this.smartAuction = new ethers.Contract(this.contractAddress, this.contractAbi, this.loginService.provider);
+    this.highestBid =  await this.smartAuction.highestBid() / this.ehtPrecision;
+    this.auctionAlreadyEnded = await this.smartAuction.auctionAlreadyEnded();
   }
 
-  async storeNumber() {
-    this.contractIstance = new ethers.Contract(this.contractAddress, this.contractAbi, this.loginService.signer);
-    await this.contractIstance.store(100);
+  public async bid(amount: number) {
+    this.smartAuction = new ethers.Contract(this.contractAddress, this.contractAbi, this.loginService.signer);
+    let amountConverted: string = (amount).toString();
+    await this.smartAuction.bid({
+      from: this.loginService.userAddress, 
+      value: parseUnits(amountConverted),
+      gasPrice: this.loginService.provider.getGasPrice(),
+      gasLimit: ethers.utils.hexlify(100000)
+    });
   }
-*/
+
+  public async withdraw() {
+    this.smartAuction = new ethers.Contract(this.contractAddress, this.contractAbi, this.loginService.signer);
+    await this.smartAuction.withdraw({
+      from: this.loginService.userAddress
+    });
+  }
+
+  public async endAuction() {
+    this.smartAuction = new ethers.Contract(this.contractAddress, this.contractAbi, this.loginService.signer);
+    await this.smartAuction.endAuction();
+  } 
 }
