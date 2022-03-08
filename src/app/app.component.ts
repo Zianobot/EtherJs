dc999797a63a31c43b2a5e9dc96fee294e0d80e5
Alfredo <graziano.alfredo34@yahoo.com>
2022-03-08 17:39:19 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { ContractService } from './contract.service';
+import { ethers } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +17,24 @@ export class AppComponent {
   userAddress: string = '';
   show: boolean = false;
 
-  constructor(private loginService: LoginService, private contractService: ContractService) {
-  }
+  //smartAuction
+  public highestBid = 0;
   
+  public amount: number = 0;
+  public auctionAlreadyEnded: boolean = false;
+
+  private ehtPrecision = 10 ** 18;
+  private smartAuction: any;
+  private account: any;
+
+
+  constructor(private loginService: LoginService, private contractService: ContractService) {}
+  
+  async ngOnInit() {
+    
+    //this.fetchCurrentValues();
+
+  }
 
   async login() {
     this.loginService.loginWithMetamask();
@@ -29,7 +46,7 @@ export class AppComponent {
     this.show = true;
     this.userAddress = this.loginService.userAddress;
   }
-
+/*
  async readContract() {
    this.contractService.getContractInfo();
   }
@@ -37,4 +54,38 @@ export class AppComponent {
  async modifyContract() {
    this.contractService.storeNumber();
  }
+
+*/
+
+  //contratto smartAuction
+
+  public async fetchCurrentValues() {
+      this.smartAuction = new ethers.Contract(this.contractService.contractAddress, this.contractService.contractAbi, this.loginService.provider);
+      this.highestBid =  await this.smartAuction.highestBid() / this.ehtPrecision;
+      this.auctionAlreadyEnded = await this.smartAuction.auctionAlreadyEnded();
+  }
+
+  public async bid() {
+    this.smartAuction = new ethers.Contract(this.contractService.contractAddress, this.contractService.contractAbi, this.loginService.signer);
+    let amountConverted: string = (this.amount).toString();
+    await this.smartAuction.bid({
+      from: this.loginService.userAddress, 
+      value: parseUnits(amountConverted),
+      gasPrice: this.loginService.provider.getGasPrice(),
+      gasLimit: ethers.utils.hexlify(100000)
+    });
+  }
+
+  public async withdraw() {
+    this.smartAuction = new ethers.Contract(this.contractService.contractAddress, this.contractService.contractAbi, this.loginService.signer);
+    let withdrawSuccess = await this.smartAuction.withdraw({
+      from: this.loginService.userAddress
+    });
+    console.log(withdrawSuccess);
+  }
+
+  endAuction() {
+
+  }
+
 }
